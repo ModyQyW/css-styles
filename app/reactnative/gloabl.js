02a087628650e9d8a1831c96f4bde3d28ac117c6
ModyQyW <261857860@qq.com>
2019-06-08 20:56:10 +0800

    update

---
+++ app/reactnative/gloabl.js
@@ -0,0 +1,112 @@
+import { StyleSheet } from 'reactnative';
+
+export default StyleSheet.create({
+  textThin: {
+    fontWeight: '100',
+  },
+  textExtraLight: {
+    fontWeight: '200',
+  },
+  textUltraLight: {
+    fontWeight: '200',
+  },
+  textLight: {
+    fontWeight: '300',
+  },
+  textRegular: {
+    fontWeight: '400',
+  },
+  textNormal: {
+    fontWeight: '400',
+  },
+  textMedium: {
+    fontWeight: '500',
+  },
+  textSemiBold: {
+    fontWeight: '600',
+  },
+  textBold: {
+    fontWeight: '700',
+  },
+  textExtraBold: {
+    fontWeight: '800',
+  },
+  textUltraBold: {
+    fontWeight: '800',
+  },
+  textBlack: {
+    fontWeight: '900',
+  },
+  textHeavy: {
+    fontWeight: '900',
+  },
+  text12: {
+    fontsize: 12,
+    lineheight: 20,
+  },
+  text13: {
+    fontsize: 13,
+    lineheight: 21,
+  },
+  text14: {
+    fontsize: 14,
+    lineheight: 22,
+  },
+  text15: {
+    fontsize: 15,
+    lineheight: 23,
+  },
+  text16: {
+    fontsize: 16,
+    lineheight: 24,
+  },
+  text17: {
+    fontsize: 17,
+    lineheight: 25,
+  },
+  text18: {
+    fontsize: 18,
+    lineheight: 26,
+  },
+  text19: {
+    fontsize: 19,
+    lineheight: 27,
+  },
+  text20: {
+    fontsize: 20,
+    lineheight: 28,
+  },
+  text21: {
+    fontsize: 21,
+    lineheight: 29,
+  },
+  text22: {
+    fontsize: 22,
+    lineheight: 30,
+  },
+  text23: {
+    fontsize: 23,
+    lineheight: 31,
+  },
+  text24: {
+    fontsize: 24,
+    lineheight: 32,
+  },
+  text25: {
+    fontsize: 25,
+    lineheight: 33,
+  },
+  text26: {
+    fontsize: 26,
+    lineheight: 34,
+  },
+  textLeft: {
+    textAlign: 'left',
+  },
+  textCenter: {
+    textalign: 'center',
+  },
+  textRight: {
+    textalign: 'right',
+  },
+});
