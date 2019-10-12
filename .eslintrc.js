1420f79285a38b1251235a5864785d56cf0f5978
ModyQyW <261857860@qq.com>
2019-10-12 16:56:41 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-  "parserOptions": {
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true
+  'parserOptions': {
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true,
+      'jsx': true
     },
   },
-  "parser": "babel-eslint",
-  "env": {
-    "es6": true,
-    "react-native/react-native": true
+  'parser': 'babel-eslint',
+  'env': {
+    'es6': true,
+    'react-native/react-native': true
   },
-  "plugins": [
-    "react",
-    "react-native-a11y",
-    "react-native"
+  'plugins': [
+    'react',
+    'react-native-a11y',
+    'react-native'
   ],
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-native-a11y/recommended",
-    "plugin:react-native/all",
-    "standard",
-    "standard-react"
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-native-a11y/recommended',
+    'plugin:react-native/all',
+    'standard',
+    'standard-react'
   ],
-  "rules": {
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "react-native/sort-styles": 0
+  'rules': {
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'react-native/sort-styles': 0
   }
-};
+}
